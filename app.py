2b1ffa1b96cb02dbdab0f811da486843e56a7c7c
theodorewright11 <theodorewrightwork@gmail.com>
2025-12-02 17:17:26 -0700

    Changed columsn read in to include notes

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ current_sheet = sheet_urls[user_choice]
 
 # --- Load that sheet ---
 try:
-    existing = conn.read(spreadsheet=current_sheet, usecols=list(range(8)))
+    existing = conn.read(spreadsheet=current_sheet)
 except Exception as e:
     st.warning(f"Could not load sheet for {user_choice}: {e}")
     existing = pd.DataFrame()
